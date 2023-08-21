651685a68948a27b230585c45201428218c1f023
Ivana <bosilkovska.ivana@gmail.com>
2023-08-21 20:16:24 +0200

    my first commit

---
+++ src/integrationTests/TestClass.java
@@ -0,0 +1,43 @@
+package integrationTests;
+
+public class TestClass {
+
+    public int getAge() {
+        return age;
+    }
+
+    public TestClass(int age) {
+        this.age = age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        sayHello("Sofija");
+        sayGoodBye("Tea");
+
+
+        // EDNA LINIJA NA KOMENTAR
+
+        System.out.println("Learning IntelliJ");
+
+    }
+
+    private int age = 5;
+
+    public static void sayHello(String name){
+        // TODO: Add support for multiple users
+        // TODO: To be run once Bug - 548 is fixed
+        System.out.println("Hello " +name);
+
+    }
+
+    public static void sayGoodBye(String name){
+        System.out.println("Good bye " +name);
+    }
+
+}
